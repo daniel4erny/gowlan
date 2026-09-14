f187b308906c36d9dcd3f46429bbafbd436fc6b8
daniel4erny <dandul.black@gmail.com>
2026-09-14 16:57:19 +0200

    uwuq

--- client/go.mod
+++ client/go.mod
@@ -1,3 +1,5 @@
 module example.com/m/v2
 
 go 1.27.1
+
+require github.com/gorilla/websocket v1.5.3 // indirect
---
+++ client/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	"fmt"
+
+	"github.com/gorilla/websocket"
+)
+
+func readLoop(conn *websocket.Conn){
+	for {
+		_, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("maaan gg:", err)
+				return
+			}
+		log.Printf("message got: %s", message)
+	}
+}
+
+func main(){
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	var host string
+	fmt.Print("Type host you want to connect to: ")
+	_, err := fmt.Scanln(&host)
+	if err != nil {
+		log.Println(err)
+	}
+
+	u := url.URL{Scheme: "ws", Host: host, Path: "/"}
+	log.Printf("connecting to: %s", u.String())
+
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close()
+
+	conn.WriteMessage(websocket.TextMessage, []byte("celkem skibodi"))
+
+	go readLoop(conn)
+
+	select{
+	case <-interrupt:
+		os.Exit(0)
+		err = conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+    	if err != nil {
+        	log.Println("Chyba při odesílání close zprávy:", err)
+        return
+    }
+	}
+}
